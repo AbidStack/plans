37dd5571feda2a3e226a5fbe236a22d27fc5c1bd
AbidStack <abidstack@gmail.com>
2023-12-03 22:33:30 +0600

    folder added

---
+++ js/header.js
@@ -0,0 +1,6 @@
+document.getElementById(`header`).innerHTML = `<img src="../img/logo.png" alt="Serever Anywhere" class="logo" height="76px" width="76px">        
+<div class="header-right">
+    <a href="hosting.html" class="header-button" target="_blank">Hositng Plans</a>
+    <a href="#contact" class="header-button">About Us</a>
+    <a href="#contact" class="header-button">Contact</a>
+</div>`
